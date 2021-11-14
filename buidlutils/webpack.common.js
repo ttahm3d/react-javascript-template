77e9cf8679fc99bb1b6809da4d5b0946fcb48bf8
ttahm3d <ttahm3d@gmail.com>
2021-11-14 14:21:25 +0530

    feat: added eslint and prettier

---
+++ buidlutils/webpack.common.js
@@ -0,0 +1,57 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "../src/index.js"),
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|ico)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(svg|ttf|eot|ttf|woff(2)?)$/i,
+        type: "asset/inline",
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, "../build"),
+    filename: "[name].[contenthash].bundle.js",
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "../public/index.html"),
+    }),
+  ],
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        node_vendors: {
+          name: "vendor",
+          test: /[\\/]node_modules[\\/]/,
+          chunks: "all",
+          priority: 1,
+        },
+      },
+    },
+  },
+};
--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 export default function App() {
-	return (
-		<div
-			style={{
-				display: "flex",
-				justifyContent: "center",
-				alignItems: "center",
-				minHeight: "100vh",
-			}}>
-			<h3>React javascript starter</h3>
-		</div>
-	);
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+      }}
+    >
+      <h3>React javascript starter</h3>
+    </div>
+  );
 }
